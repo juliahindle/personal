cf52d13690da33f415541f751159ce6656a7a4c6
Julia <52930474+juliahindle@users.noreply.github.com>
2020-10-05 13:57:15 -0400

    first push

---
+++ glazes.js
@@ -0,0 +1,38 @@
+var jsonObject = 
+{ 
+  "1-1-1":
+  {
+    "date": "12/7/19",
+    "name": "Randy's Green",
+    "base recipe":
+    {
+      "Gerstley Borate": 31.7,
+      "Talc": 13.8,
+      "F-4 Feldspar": 19.8,
+      "EP Kaolin": 5,
+      "Silica": 29.7
+    },
+    "add":
+    {
+      "Chromium Oxide": 0.15
+    }
+  },
+  "13-1-1":
+  {
+    "date": "10/1/20",
+    "name": "Heino",
+    "base recipe":
+    {
+      "Nepheline Syenite": 57.5,
+      "Silica": 7.1,
+      "Ball Clay": 6.2,
+      "Barium Carbonate": 26.5,
+      "Lithium Carbonate": 2.7
+    },
+    "add":
+    {
+      "Copper Carbonate": 3,
+      "Bentonite": 2
+    }
+  }
+}
