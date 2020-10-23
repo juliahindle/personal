a8620fdd7333818ade0bb2628c71cac60bb1cc86
Julia <52930474+juliahindle@users.noreply.github.com>
2020-10-23 02:45:38 -0400

    glaze page

--- glazes.js
+++ glazes.js
@@ -5,6 +5,18 @@ var jsonObject =
     "date": "12/7/19",
     "name": "Randy's Green",
     "base recipe":
+      [["Gerstley Borate", "Talc", "F-4 Feldspar", "EP Kaolin","Silica"],
+       [31.7, 13.8, 19.8, 5, 29.7]]
+    "add": 
+      [["Chromium Oxide"],
+       [0.15]]
+    }
+  },
+  "1-24":
+  {
+    "date": "10/1/20",
+    "name": "Randy's Red",
+    "base recipe":
     {
       "Gerstley Borate": 31.7,
       "Talc": 13.8,
@@ -14,13 +26,13 @@ var jsonObject =
     },
     "add":
     {
-      "Chromium Oxide": 0.15
+      "Red Iron Oxide": 15
     }
   },
   "13-1-1":
   {
     "date": "10/1/20",
-    "name": "Heino",
+    "name": "Heino (Original 1)",
     "base recipe":
     {
       "Nepheline Syenite": 57.5,
@@ -32,6 +44,255 @@ var jsonObject =
     "add":
     {
       "Copper Carbonate": 3,
+      "Cobalt Carbonate": 1,
+      "Bentonite": 2
+    }
+  },
+  "13-1-2":
+  {
+    "date": "10/1/20",
+    "name": "Heino (Original 2)",
+    "base recipe":
+    {
+      "Nepheline Syenite": 57.5,
+      "Silica": 7.1,
+      "Ball Clay": 6.2,
+      "Barium Carbonate": 26.5,
+      "Lithium Carbonate": 2.7
+    },
+    "add":
+    {
+      "Copper Carbonate": 3,
+      "Cobalt Carbonate": 2,
+      "Bentonite": 2
+    }
+  },
+  "13-2-1":
+  {
+    "date": "10/1/20",
+    "name": "Heino (Strontium 1)",
+    "base recipe":
+    {
+      "Nepheline Syenite": 61.58,
+      "Silica": 7.6,
+      "Ball Clay": 6.64,
+      "Barium Carbonate": 19.875,
+      "Lithium Carbonate": 2.7
+    },
+    "add":
+    {
+      "Copper Carbonate": 3,
+      "Cobalt Carbonate": 1,
+      "Bentonite": 2
+    }
+  },
+  "13-2-2":
+  {
+    "date": "10/1/20",
+    "name": "Heino (Strontium 2)",
+    "base recipe":
+    {
+      "Nepheline Syenite": 61.58,
+      "Silica": 7.6,
+      "Ball Clay": 6.64,
+      "Barium Carbonate": 19.875,
+      "Lithium Carbonate": 2.7
+    },
+    "add":
+    {
+      "Copper Carbonate": 3,
+      "Cobalt Carbonate": 2,
+      "Bentonite": 2
+    }
+  },
+  "11-1":
+  {
+    "date": "11/15/19",
+    "name": "Chun Seafoam (Limestone)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Limestone": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Copper Carbonate": 5,
+      "Titanium Dioxide": 5,
+      "Bentonite": 2
+    }
+  },
+  "11-2":
+  {
+    "date": "11/15/19",
+    "name": "Chun Seafoam (Whiting 1)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Copper Carbonate": 5,
+      "Titanium Dioxide": 5,
+      "Bentonite": 2
+    }
+  },
+  "11-3":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Neph Sye)",
+    "base recipe":
+    {
+      "Nepheline Syenite": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Copper Carbonate": 5,
+      "Titanium Dioxide": 5,
+      "Bentonite": 2
+    }
+  },
+  "11-4":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Mod 1)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Copper Oxide": 4.6,
+      "Chrome Oxide": 1.5,
+      "Bentonite": 2
+    }
+  },
+  "11-5":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Mod 2)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Rutile (Light)": 3.7,
+      "Cobalt Carbonate": 0.3,
+      "Copper Oxide": 0.8,
+      "Bentonite": 2
+    }
+  },
+  "11-6":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Mod 3)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Cobalt Carbonate": 4,
+      "Titanium Dioxide": 5,
+      "Bentonite": 2
+    }
+  },
+  "11-7":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Mod 4)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Chromium Oxide": 0.15,
+      "Titanium Dioxide": 5,
+      "Bentonite": 2
+    }
+  },
+  "11-8":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Mod 5)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Chromium Oxide": 0.15,
+      "Bentonite": 2
+    }
+  },
+  "11-9":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Mod 6)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Copper Carbonate": 5,
+      "Titanium Dioxide": 5,
+      "Cobalt Carbonate": 3,
+      "Bentonite": 2
+    }
+  },
+  "11-10":
+  {
+    "date": "10/1/20",
+    "name": "Chun Seafoam (Whiting 2)",
+    "base recipe":
+    {
+      "F-4 Feldspar": 41,
+      "Silica": 27,
+      "Whiting": 15,
+      "EP Kaolin": 5,
+      "Zinc Oxide": 12
+    },
+    "add":
+    {
+      "Copper Carbonate": 5,
+      "Titanium Dioxide": 5,
       "Bentonite": 2
     }
   }
